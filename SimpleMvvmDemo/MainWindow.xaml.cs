4ee174aa0b57d7cffab547ea39243ad3a9f55118
zhouxiao123456 <15996164069@163.com>
2020-09-10 09:20:35 +0800

    MVVM实例

---
+++ SimpleMvvmDemo/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+/*
+namespace SimpleMvvmDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            double d1 = double.Parse(this.tb1.Text);
+            double d2 = double.Parse(this.tb2.Text);
+            double result = d1 + d2;
+            this.tb3.Text = result.ToString();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.ShowDialog();
+        }
+    }
+}
+*/
+
+
+namespace SimpleMvvmDemo
+{
+    /// <summary>
+    /// 更改需求
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.slider3.Value = this.slider1.Value + slider2.Value;
+        }
+
+        private void saveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.ShowDialog();
+        }
+    }
+}
